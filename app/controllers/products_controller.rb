c5be470f9cf2cfe46e7b11175f526497ae63d743
hectorgool <hectorgool@gmail.com>
2016-12-20 07:50:56 -0600

    add button products on views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,10 @@ class ProductsController < ApplicationController
 
 	def index
     @products = Product.last(12).reverse
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @products }
+    end
   end
 
   def show
--- app/views/products/index.json.jbuilder
+++ app/views/products/index.json.jbuilder
@@ -1,3 +1,4 @@
+#http://localhost:3000/products.json
 json.products @products do |product|
   json.name product.name
   json.description product.description
