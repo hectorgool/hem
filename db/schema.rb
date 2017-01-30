5ab3f74e7430959ddb1053e59be1248effbcfb85
Hector Gonzalez <hector.gonzalez@enova.mx>
2017-01-30 17:10:25 -0600

    fix edit migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130162440) do
+ActiveRecord::Schema.define(version: 20170130230010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,9 +39,10 @@ ActiveRecord::Schema.define(version: 20170130162440) do
     t.boolean  "approved",                            default: false
     t.integer  "user_id"
     t.decimal  "price",       precision: 8, scale: 2
-    t.string   "category_id"
+    #t.integer  "category_id"
   end
 
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
   add_index "products", ["user_id"], name: "index_products_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
@@ -67,5 +68,6 @@ ActiveRecord::Schema.define(version: 20170130162440) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "attachments", "products"
+  add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
 end
