82de44fe5d93894a3d6b5d9dbf8392b1e2a41e2c
Hector Gonzalez <hector.gonzalez@enova.mx>
2017-01-31 18:03:20 -0600

    local storage for dev

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -8,7 +8,9 @@ class AttachmentUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  # storage :fog
+  # santo
+  storage (Rails.env.production? ? :fog : :file)
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
